b0dc1a309e70ed7c0b22c7697f14ea2af1101a61
indiportillo <indi.portillo@gmail.com>
2017-06-28 09:35:26 +0200

    finishes day 1

---
+++ Iñigo Portillo/configuration.R
@@ -0,0 +1,4 @@
+DATA_DIR  <- file.path("..", "data")
+INSTACART_DATA_DIR <- file.path("..", "instacart_data")
+FOOTBALL_DATA_DIR <- file.path("..", "football_data")
+setwd("/media/inigo.portillo/KINGSTON/CLASE/Summer School/Big data management/big_data_with_R/Iñigo Portillo/")
